77b3bc3773f5d3f7ab6a2e625812015f877c20bf
sparrowgtt <sparrowgtt@gmail.com>
2020-01-20 03:33:03 +0300

    add project files

---
+++ conftest.py
@@ -0,0 +1,44 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    # default browser is chrome
+    # you can change it directly in params
+    # or type as a command
+
+    parser.addoption('--language', action='store', default='ru',
+                   help="Choose language: ru, en, fr, es")
+    # default language is russian
+    # you can change it the same way as a browser's name
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+            print("\nstart chrome browser for test..")
+            options = Options()
+            options.add_experimental_option('prefs', {'intl.accept_languages': language})
+            browser = webdriver.Chrome(options=options)
+    # lang params for chrome setup
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    # lang params for firefox
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    time.sleep(30)
+    print("\nquit browser..")
+    browser.quit()
