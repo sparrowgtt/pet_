a85745f4db902fb91efeff3b35d092d87f78c0d4
sparrowgtt <sparrowgtt@gmail.com>
2020-01-23 18:49:57 +0300

    modify files

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -13,3 +16,24 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+# def test_guest_should_see_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
+
+
+# def test_guest_should_see_login_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_form()
+
+#
+# def test_guest_should_see_register_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_register_form()
