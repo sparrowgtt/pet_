0afca60e2c40c0ca0798a9dcb9849adce15326d2
sparrowgtt <sparrowgtt@gmail.com>
2020-05-22 00:23:21 +0300

    final preparations for stepik according to unit 4.3.14

--- conftest.py
+++ conftest.py
@@ -39,6 +39,5 @@ def browser(request):
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    time.sleep(10)
     print("\nquit browser..")
     browser.quit()
